7485d1b9d82cce94ce3aa9e4adddae01ace36bb1
Elmir_Musaev <worldrap4@gmail.com>
2024-10-20 23:40:41 +0400

    Day00/ex00 ready

---
+++ SQL_intensive_s21/Day00/src/1_run_query.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#This script makes process of testing a sql-query easier.
+#So, there is no need to log in psql-console.
+#The script makes everything for us.
+
+#If doesn't work then try 'sudo chmod o+x 1_run_query.sh' 
+
+
+file=$1  #choose a file which contains query (f.e. bash Day00/src/ex00/day00_ex00.sql)
+psql -U sql_elmir -d sql_elmir -f $file  #DB console runs a command from a file (psql --user x --db_name x --filename)
