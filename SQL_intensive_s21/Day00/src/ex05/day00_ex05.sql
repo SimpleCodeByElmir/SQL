5a3696ee918984976a3c9126b7df63ca7fccdcd1
Elmir_Musaev <worldrap4@gmail.com>
2024-10-31 02:38:47 +0400

    ex05 done

--- SQL_intensive_s21/Day00/src/ex05/day00_ex05.sql
+++ SQL_intensive_s21/Day00/src/ex05/day00_ex05.sql
@@ -1,5 +1,16 @@
-SELECT name,
-  (SELECT person_id FROM person_order
-  WHERE (menu_id=13 OR menu_id=14 OR menu_id=18) AND order_date='2022-01-07' LIMIT 1) AS p
-FROM person
-WHERE person.id=p.person_id;
+SELECT
+    (SELECT name
+    FROM person AS p
+    WHERE p.id=o.person_id) AS NAME
+FROM person_order AS o
+WHERE (o.menu_id=13 OR o.menu_id=14 or o.menu_id=18) AND o.order_date='2022-01-07';
+
+--SELECT
+--    (SELECT name
+--    FROM person AS p
+--    WHERE (o.menu_id=13 OR o.menu_id=14 or o.menu_id=18) AND o.order_date='2022-01-07' AND p.id=o.person_id) AS NAME
+--FROM person_order AS o;
+
+--SELECT name
+--FROM person AS p, person_order AS o
+--WHERE (o.menu_id=13 OR o.menu_id=14 or o.menu_id=18) AND o.order_date='2022-01-07' AND p.id=o.person_id;
